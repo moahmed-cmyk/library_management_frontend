bd435b677f00b37a22bc16980ad2408b3d9c1334
moahmed-fahad <crfahad132@gmail.com>
2025-07-03 21:29:29 +0530

    set guard for alll compoent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,16 @@ import { BooksComponent } from './books/books.component';
 import { StudentsComponent } from './students/students.component';
 import { PaymentComponent } from './payment/payment.component';
 import { FineComponent } from './fine/fine.component';
+import { LoginGuard } from './guards/login.guard';
 
 export const routes: Routes = [
     {path:"",component:LoginPageComponent},
     {path:"register",component:RegisterPageComponent},
   { path: 'login', component: LoginPageComponent},
-    {path:"stock",component:StocksPageComponent},
-    {path:"genre",component:HomeComponent},
-    {path:"books",component:BooksComponent}, 
-    {path:"reservation",component:StudentsComponent}, 
-    {path:"payment",component:PaymentComponent}, 
-    {path:"borrowRecords",component:FineComponent}, 
+    {path:"stock",component:StocksPageComponent,canActivate:[LoginGuard]},
+    {path:"genre",component:HomeComponent,canActivate:[LoginGuard]},
+    {path:"books",component:BooksComponent,canActivate:[LoginGuard]}, 
+    {path:"reservation",component:StudentsComponent,canActivate:[LoginGuard]}, 
+    {path:"payment",component:PaymentComponent,canActivate:[LoginGuard]}, 
+    {path:"borrowRecords",component:FineComponent,canActivate:[LoginGuard]}, 
 ];
