300991a112eba3d65c92a2b54e79e0d36d9edd11
moahmed-fahad <crfahad132@gmail.com>
2025-07-04 18:00:29 +0530

    add function

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginGuard implements CanActivate {
+export class LoginGuard  {
 
   constructor(private router: Router) {}
 
   canActivate(): boolean {
-    const token = localStorage.getItem('token');
-    if (token) {
-      this.router.navigate(['/home']);
+    const user = JSON.parse(localStorage.getItem('userLoginDetail') || '{}');
+    if (user && user.account === true) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
       return false;
     }
-    return true;
   }
+
+
+
 }
 
